fb233d78ff95e69f4a269f701adb8ce03719bd60
Jory <joryakuhata@gmail.com>
2026-06-24 15:09:06 +1200

    sets up basic maramataka generator [ci]

---
+++ maramataka-domain/src/lib/maramataka-month-generator.ts
@@ -0,0 +1,30 @@
+import { Mata, MaramatakaVersion } from './mata';
+import { MaramatakaMonth } from './maramataka';
+
+interface GenerateMaramatakaMonthInput {
+  version: MaramatakaVersion;
+  whiroStartsAt: Date;
+  mata: Mata[];
+  sunsets: Date[];
+}
+
+export function generateMaramatakaMonth(
+  input: GenerateMaramatakaMonthInput
+): MaramatakaMonth {
+  if (input.sunsets.length < input.mata.length + 1) {
+    throw new Error('Not enough sunsets to generate Maramataka month');
+  }
+
+  if (input.sunsets[0].getTime() !== input.whiroStartsAt.getTime()) {
+    throw new Error('Whiro start must match first sunset');
+  }
+  return {
+    version: input.version,
+    whiroStartsAt: input.whiroStartsAt,
+    nights: input.mata.map((mata, index) => ({
+      mata,
+      startsAt: input.sunsets[index],
+      endsAt: input.sunsets[index + 1],
+    })),
+  };
+}
